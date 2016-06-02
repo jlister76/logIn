2bb0617072bf8ff1e1d64f212c50953e98ade501
John <jlister76@gmail.com>
2016-06-01 23:42:39 -0500

    error msg invalid un or pw

--- client/auth/authController.js
+++ client/auth/authController.js
@@ -3,7 +3,7 @@
 
   angular
     .module('logInApp')
-    .controller('AuthLoginController', function ($scope, $state, AuthService, $location) {
+    .controller('AuthLoginController', function ($scope, $state, AuthService, $location, $log) {
 
       $scope.user = {
         email: null,
@@ -12,12 +12,19 @@
 
       $scope.login = function () {
         AuthService.login($scope.user.email, $scope.user.password)
+          .catch(function(e){
+            if (e) {
+              console.error(e);
+              $scope.err = e;
+            }
+          })
           .then(function () {
+
             var next = $location.nextAfterLogin || '/';
             $location.nextAfterLogin = null;
             $location.path(next);
             $state.go('/main');
-          });
+          })
       };
     })
     .controller('AuthLogoutController', function ($scope, $state, AuthService) {
